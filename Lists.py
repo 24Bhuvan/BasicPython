dcef76e2d6a7a6d8b180e4a5443f5cce5235c5e8
Bhuvan Ummidisetti <ubhuvan533@gmail.com>
2024-12-20 21:09:39 +0530

    Update Lists.py
    Corrected index for second name.

--- Lists.py
+++ Lists.py
@@ -6,7 +6,7 @@ numbers.append(2)
 numbers.append(3)
 strings.append('hello')
 strings.append('world')
-second_name = names[2]
+second_name = names[1]
 print(numbers)
 print(strings)
 print("The second name on the names list is %s" % second_name).
